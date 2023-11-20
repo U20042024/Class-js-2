c568813e296b4bd44d0b38f4a3ec594228435c6a
Oussama Bouna <147092989+U20042024@users.noreply.github.com>
2023-11-20 18:05:51 +0100

    Add files via upload
    this code is showing some information of a Bugatti car .

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        car first_car = new car();
+        first_car.brand = "bugatti";
+        first_car.color = "Black";
+        first_car.model = "2015";
+        first_car.speed = 360;
+        first_car.nitroSpeed = true;
+        first_car.info();
+    }
+}
---
+++ car.java
@@ -0,0 +1,11 @@
+public class car {
+    String brand;
+    String color;
+    String model;
+    Short speed;
+    Boolean nitroSpeed;
+
+    void info() {
+        System.out.println("This car's name is " + brand + ", it is " + color + ", its model is " + model + ", its max speed is " + speed + "and its nitroSpeed is " + nitroSpeed );
+    }
+}
